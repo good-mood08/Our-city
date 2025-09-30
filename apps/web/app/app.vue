69233d51b41a52bcb369316a820850d11ed5425d
Akzis <akzi31xx@gmail.com>
2025-09-30 22:36:14 +0300

    hero_heder

--- apps/web/app/app.vue
+++ apps/web/app/app.vue
@@ -2,25 +2,32 @@
 
 </script>
 
-<template >
-  <div class="gap">
-    
-    
-    <selection>
-      <appeal  v-for="value in 4"/>
-    </selection>
-    <selection>
-      <request status_variant="red" v-for="value in 4"></request>
-    </selection>
+<template>
+  <heder />
 
-   
-  
+    <hero />
+
+  <div class="gap">
+      <selection>
+        <appeal v-for="value in 4" />
+      </selection>
+      <selection>
+        <request status_variant="red" v-for="value in 4" />
+      </selection>
   </div>
 </template>
+
 <style scoped>
 .gap {
   display: grid;
   gap: 15px;
+  
   padding: 20px;
 }
+
+
+
+
 </style>
+
+
