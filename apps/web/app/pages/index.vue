b7d02fb9bba3cb4b7f20a3a6d8d055cca53950f2
Akzi <166204693+Akzis@users.noreply.github.com>
2025-10-03 18:18:14 +0300

    ButtonColorApplication

--- apps/web/app/pages/index.vue
+++ apps/web/app/pages/index.vue
@@ -3,6 +3,19 @@
   <hero />
   <div class="flex flex-col g-4 p-5">
     
+  <div class="flex flex-row gap-4">
+    <buttonApplication 
+      title="Подать заявку" 
+      description="Сообщить о проблеме" 
+      color="red"
+    />
+    <buttonApplication 
+      title="Мои заявки" 
+      description="Сообщить о проблеме" 
+      color="blue"
+    />
+  </div>
+
 
     <selection>
       <appeal :number="100" v-for="value in 4"/>
