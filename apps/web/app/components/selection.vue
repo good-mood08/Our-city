142a053e2236fb44cbbe63743f009d11f11dbd5a
derloky <semenpostnikov093@gmail.com>
2025-10-13 20:03:31 +0300

    sub

--- apps/web/app/components/selection.vue
+++ apps/web/app/components/selection.vue
@@ -3,6 +3,10 @@ const props = defineProps({
     title: {
         type: String,
         default:'Заголовок' 
+    },
+        svg: {
+        type:String,
+        
     }
 })
 </script>
@@ -10,7 +14,10 @@ const props = defineProps({
 
 <template>
 <div class=" flex py-5 flex-col gap-3 justify-start  ">
-    <h3 class=" text-stone-950 t-xl">{{ props.title }}</h3>
+    <div class="flex gap-2.5">
+        <img :src="props.svg" alt="" class="w-5">
+        <h3 class=" text-stone-950 t-xl">{{ props.title }}</h3>
+    </div>
     <slot></slot>
 </div>
 </template>
---
+++ apps/web/app/pages/submitting.vue
@@ -0,0 +1,45 @@
+
+<script setup lang="ts">
+const items = ref(['Backlog', 'Todo', 'In Progress', 'Done'])
+const categ = ref(['Apple', 'Banana', 'Blueberry', 'Grapes', 'Pineapple'])
+const value = ref('')
+
+
+const state = reactive({
+  email: '',
+  password: '',
+  value: ''
+})
+
+const toast = useToast()
+async function onSubmit(event:any) {
+  toast.add({ title: 'Success', description: 'The form has been submitted.', color: 'success' })
+  console.log(event.data)
+}
+</script>
+
+<template>
+    <div class="px-5 py-5 ">
+        <UForm :state="state" class="flex flex-col gap-2.5 " @submit="onSubmit">
+            <selection title="msdmdsad" svg="/vectors/circle-alert.svg" >
+                <UFormField label="Email" name="email"  class="w-full">
+                  <USelectMenu v-model="value" :items="categ" class="w-full"/>
+                </UFormField>
+                  <UFormField label="Name" name="name"  class="w-full">
+                    <UTextarea v-model="state.value" autoresize  class="w-full"/>
+                  </UFormField>
+                <UFormField label="Password" name="password" class="w-full">
+                   <UInput v-model="state.password" type="password" class="w-full" />
+                </UFormField>
+                <UFormField label="Name" name="name"  class="w-full">
+                  <USelectMenu placeholder="Select status" :items="items" class="w-full" />
+                </UFormField>
+            </selection>
+    
+            <UButton type="submit">
+               Submit
+            </UButton>
+        </UForm>
+
+    </div>
+</template>
